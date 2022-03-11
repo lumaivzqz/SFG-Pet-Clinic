426709b018e3bac7fd4629a4004c2d22c3d4fc56
luvzqz <lucivazquez@santandertecnologia.com.ar>
2022-03-10 22:37:06 -0300

    Implement map based services

---
+++ pet-clinic-data/src/main/java/com/lumaivzqz/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.lumaivzqz.sfgpetclinic.services.map;
+
+import com.lumaivzqz.sfgpetclinic.model.Pet;
+import com.lumaivzqz.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+}
